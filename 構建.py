da1f4b231665809fef3d3d370b10729979e119cc
Rimo <RimoChan@stu.xmu.edu.cn>
2018-09-04 11:41:22 +0800

    稍微停一下

--- 構建.py
+++ 構建.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 
 f=tempfile.NamedTemporaryFile(delete=False)
-f.write(b'import os;os.system(\'\""./python36/python.exe\"" -m Librian.py\')')
+f.write(b'import os;os.system(\'\""./python36/python.exe\"" -m Librian.py\');os.system("pause")')
 f.close()
 
 os.system(f'pyinstaller {f.name} --name Librian.exe --distpath ./ -F -i 資源/librian.ico')
