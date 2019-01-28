34e15b84f89e37c782a06857d204b88c411e6d96
Rimo <RimoChan@stu.xmu.edu.cn>
2019-01-28 12:12:30 +0800

    美化了一下代碼

--- 導出pdf.py
+++ 導出pdf.py
@@ -7,73 +7,76 @@ import opencc
 
 import 編譯
 
-此處=os.path.dirname(os.path.abspath(__file__))
+此處 = os.path.dirname(os.path.abspath(__file__))
 
 cc = opencc.OpenCC('t2s')
 簡化字 = True
 
 參數 = argparse.ArgumentParser(description='劇本文件生成pdf')
 參數.add_argument('--css', type=str, default='./導出組件/樣式.html')
-參數.add_argument('--play' , type=str, required=True)
+參數.add_argument('--play', type=str, required=True)
 參數.add_argument('--out', type=str)
 參數 = 參數.parse_args()
 
-with open(os.path.join(此處,參數.css),encoding='utf8') as f:
-    頭=f.read()
+with open(os.path.join(此處, 參數.css), encoding='utf8') as f:
+    頭 = f.read()
+
 
 def 化(s):
     if 簡化字:
         return cc.convert(s)
     return s
 
-def 打標點(句): 
+
+def 打標點(句):
     句 = re.sub('([，。？！…——])', '<span class="標點">\\1</span>', 句)
     句 = re.sub('(「.*?」)', '<span class="引用">\\1</span>', 句)
     return 句
 
+
 def 導出(f):
-    html=頭
+    html = 頭
     a = 編譯.編譯(f)
     for s in a:
         if '插入圖' in s:
-            html+=f'<hr/>{s["插入圖"]}<hr/>'
+            html += f'<hr/>{s["插入圖"]}<hr/>'
         if '旁白' in s:
-            旁白=打標點(化(s["旁白"]))
-            html+=f'<span class="旁白">　　{旁白}</span><br/>\n'
+            旁白 = 打標點(化(s["旁白"]))
+            html += f'<span class="旁白">　　{旁白}</span><br/>\n'
         if '名' in s:
             代 = 化(s['代'] or s["名"])
             顏 = 化(s['顏'] or '')
-            語 = 化(s.get('語',''))
-            html+=f'<span class="代 {s["名"]}">　　{代}</span>'
+            語 = 化(s.get('語', ''))
+            html += f'<span class="代 {s["名"]}">　　{代}</span>'
             if 顏:
-                html+=f'<span class="顏">({顏})</span>'
+                html += f'<span class="顏">({顏})</span>'
             if 語:
-                html+=f'「<span class="語">{語}</span>」'
-            html+=f'<br/>'
-            
-                
+                html += f'「<span class="語">{語}</span>」'
+            html += f'<br/>'
+
         if '註釋' in s:
-            html+=f'<span class="註釋">{s["註釋"]}</span><br/>\n'
+            html += f'<span class="註釋">{s["註釋"]}</span><br/>\n'
         if '空行' in s:
-            html+='<br/>\n'
-            
+            html += '<br/>\n'
+
     return html
-    
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     if 參數.play:
-        pdf=html=False
+        pdf = html = False
         if 參數.out is None:
-            參數.out=參數.play[:-5]+'.pdf'
-            pdf=True
+            參數.out = 參數.play[:-5] + '.pdf'
+            pdf = True
         elif 參數.out.endswith('pdf'):
-            pdf=True
+            pdf = True
         elif 參數.out.endswith('pdf'):
-            html=True
-            
-        with open(參數.play,encoding='utf8') as f:
-            t=導出(f)
+            html = True
+
+        with open(參數.play, encoding='utf8') as f:
+            t = 導出(f)
             if html:
-                with open(參數.out,'w',encoding='utf8') as f2:
+                with open(參數.out, 'w', encoding='utf8') as f2:
                     f2.write(導出(f))
             if pdf:
                 pdfkit.from_string(t, 參數.out, options={
@@ -82,6 +85,6 @@ if __name__=='__main__':
                     'margin-bottom': '0in',
                     'margin-left': '0in',
                     'encoding': 'UTF-8',
-                    })
+                })
             else:
                 raise Exception('操你媽')
--- 窗口.py
+++ 窗口.py
@@ -1,31 +1,38 @@
+import logging
+import json
+import os
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebChannel import QWebChannel
 from PyQt5.QtGui import *
-import logging
-import json
-import os
 
 import 劇本
-from 環境 import 配置,工程路徑
+from 環境 import 配置, 工程路徑
 
-讀者=劇本.讀者(f'{工程路徑}/{配置["劇本入口"]}')
+
+讀者 = 劇本.讀者(f'{工程路徑}/{配置["劇本入口"]}')
+
 
 def js(code):
     主窗口.網頁.頁面.runJavaScript(code)
+
+
 def 更新():
-    狀態=讀者.狀態.導出()
+    狀態 = 讀者.狀態.導出()
     if 狀態['js']:
         js(狀態['js'])
-        讀者.狀態.js=''
+        讀者.狀態.js = ''
     js('演出.改變演出狀態(%s)' % json.dumps(狀態))
-#————————————————————————————
-#接受gui回傳的資訊
+
+
+# ————————————————————————————
+# 接受gui回傳的資訊
 class 山彥(QObject):
-    def __init__(self,頁面):
+    def __init__(self, 頁面):
         super().__init__()
-        self.頁面=頁面
+        self.頁面 = 頁面
 
     def 選擇讀檔文件(self):
         return QFileDialog.getOpenFileName(
@@ -34,6 +41,7 @@ class 山彥(QObject):
             f'{工程路徑}/存檔資料',
             'pickle 文件 (*.pkl)'
         )
+
     def 選擇存檔文件(self):
         return QFileDialog.getSaveFileName(
             self.頁面.parent(),
@@ -41,20 +49,23 @@ class 山彥(QObject):
             f'{工程路徑}/存檔資料',
             'pickle 文件 (*.pkl)'
         )
-        
+
     def 更新(self):
         更新()
+
     def 步進(self):
         讀者.步進()
+
     def 步進更新(self):
         讀者.步進()
         更新()
+
     def 初始化(self):
-        圖片文件夾=os.path.join(f'../{工程路徑}',配置['圖片文件夾']).replace('\\','/')
-        音樂文件夾=os.path.join(f'../{工程路徑}',配置['音樂文件夾']).replace('\\','/')
-        自定css=os.path.join(f'../{工程路徑}',配置['自定css']).replace('\\','/')
-        主題css=os.path.join(f'主題',配置['主題css']+'.css').replace('\\','/')
-        s=f'''
+        圖片文件夾 = os.path.join(f'../{工程路徑}', 配置['圖片文件夾']).replace('\\', '/')
+        音樂文件夾 = os.path.join(f'../{工程路徑}', 配置['音樂文件夾']).replace('\\', '/')
+        自定css = os.path.join(f'../{工程路徑}', 配置['自定css']).replace('\\', '/')
+        主題css = os.path.join(f'主題', 配置['主題css'] + '.css').replace('\\', '/')
+        s = f'''
               解析度={配置['主解析度']};
               邊界={int(配置['顯示繪圖邊界'])};
               link_on=true;
@@ -65,64 +76,76 @@ class 山彥(QObject):
               演出.準備工作();
            '''
         try:
-            with open(f'{工程路徑}/存檔資料/用戶設置.json',encoding='utf8') as f:
-                s+=f"設置.應用用戶設置('{f.read()}');"
+            with open(f'{工程路徑}/存檔資料/用戶設置.json', encoding='utf8') as f:
+                s += f"設置.應用用戶設置('{f.read()}');"
         except:
             logging.warning('用戶設置加載失敗')
         js(s)
+
     def 存檔(self):
         文件名, 文件類型 = self.選擇存檔文件()
         if 文件名:
             讀者.存檔(文件名)
             js('演出.提示("存檔好了。")')
+
     def 讀檔(self):
         文件名, 文件類型 = self.選擇讀檔文件()
         if 文件名:
             讀者.讀檔(文件名)
             更新()
+
     def 快速存檔(self):
         讀者.存檔(f'{工程路徑}/存檔資料/快速存檔.pkl')
         js('演出.提示("存檔好了。")')
+
     def 快速讀檔(self):
         讀者.讀檔(f'{工程路徑}/存檔資料/快速存檔.pkl')
         更新()
+
     def 從title讀檔(self):
         文件名, 文件類型 = self.選擇讀檔文件()
         if 文件名:
             讀者.讀檔(文件名)
             js('開始()')
-    def 從劇本開始(self,劇本):
-        入口=f'{工程路徑}/{配置["劇本入口"]}'
+
+    def 從劇本開始(self, 劇本):
+        入口 = f'{工程路徑}/{配置["劇本入口"]}'
         讀者.__init__(f'{os.path.dirname(入口)}/{劇本}')
         js('link_on=true;開始();')
+
     def 退出(self):
         exit()
+
     def 回標題(self):
         讀者.__init__()
         js(f'window.location.href="file:///{工程路徑}/{配置["標題畫面"]}"')
-    def 切換全屏(self): 
+
+    def 切換全屏(self):
         主窗口.切換全屏()
-    def 設置(self,參數):
-        with open(f'{工程路徑}/存檔資料/用戶設置.json','w',encoding='utf8') as f:
+
+    def 設置(self, 參數):
+        with open(f'{工程路徑}/存檔資料/用戶設置.json', 'w', encoding='utf8') as f:
             f.write(參數)
-    def 選(self,參數):
-        t=讀者.狀態.選項[int(參數)][1]
-        讀者.狀態.選項=()
+
+    def 選(self, 參數):
+        t = 讀者.狀態.選項[int(參數)][1]
+        讀者.狀態.選項 = ()
         t()
-        logging.debug('選擇了「%s」。'%參數)
+        logging.debug('選擇了「%s」。' % 參數)
         讀者.步進()
         更新()
-        
-    @pyqtSlot(str,str)
-    def rec2(self,令,參數):
+
+    @pyqtSlot(str, str)
+    def rec2(self, 令, 參數):
         logging.debug(f'收到頁面來的指令:「{令}({參數})」')
         if 令 in 山彥.__dict__:
-            山彥.__dict__[令](self,參數)
+            山彥.__dict__[令](self, 參數)
         else:
             raise Exception(f'命令「{令}」無法理解。')
         js('link_on=true')
+
     @pyqtSlot(str)
-    def rec1(self,令):
+    def rec1(self, 令):
         logging.debug(f'收到頁面來的指令: 「{令}」')
         if 令 in 山彥.__dict__:
             山彥.__dict__[令](self)
@@ -130,20 +153,21 @@ class 山彥(QObject):
             raise Exception(f'命令「{令}」無法理解。')
         js('link_on=true')
 
-#————————————————————————————
-#視窗介面
+
+# ————————————————————————————
+# 視窗介面
 class gal窗口(QWebEngineView):
 
-    def __init__(self,*li):
+    def __init__(self, *li):
         super().__init__(*li)
         self.準備網頁()
         self.做界面()
 
-    def 準備網頁(self): 
-        self.頁面=self.page()
+    def 準備網頁(self):
+        self.頁面 = self.page()
         self.頻道 = QWebChannel()
         self.handler = 山彥(self.頁面)
-        self.頻道.registerObject('handler',self.handler)
+        self.頻道.registerObject('handler', self.handler)
         self.頁面.setWebChannel(self.頻道)
 
     def 做界面(self):
@@ -153,43 +177,43 @@ class gal窗口(QWebEngineView):
             self.load(QUrl(f'file:///{工程路徑}/{配置["標題畫面"]}'))
         else:
             self.load(QUrl('file:///html/默認標題畫面/標題.html'))
-        
+
         self.show()
+
 
 class 統合窗口(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle(配置['標題'])
         if 配置['圖標']:
-            self.setWindowIcon(QIcon('%s/%s' %(工程路徑,配置['圖標']) ))
-        else: 
+            self.setWindowIcon(QIcon('%s/%s' % (工程路徑, 配置['圖標'])))
+        else:
             self.setWindowIcon(QIcon('./資源/librian.ico'))
         self.resize(*配置['主解析度'])
-        self.全屏=False
+        self.全屏 = False
         self.準備快速鍵()
-        self.網頁=gal窗口(self)
+        self.網頁 = gal窗口(self)
         self.show()
 
     def 切換全屏(self):
-        self.全屏=not self.全屏
+        self.全屏 = not self.全屏
         if self.全屏:
             self.showFullScreen()
         else:
             self.showNormal()
-    
+
     def keyPressEvent(self, event):
         keyEvent = QKeyEvent(event)
-        if keyEvent.key() in (Qt.Key_Enter,16777220):
+        if keyEvent.key() in (Qt.Key_Enter, 16777220):
             if QApplication.keyboardModifiers() == Qt.AltModifier:
                 self.切換全屏()
-    
+
     def 準備快速鍵(self):
-        QShortcut(QKeySequence('Alt+Enter'), self).activated.connect(lambda:self.切換全屏())
+        QShortcut(QKeySequence('Alt+Enter'), self).activated.connect(lambda: self.切換全屏())
 
-    def resizeEvent(self,ev):
-        self.網頁.resize(self.width(),self.height())
-        self.網頁.頁面.setZoomFactor(min(self.width()/配置['主解析度'][0],self.height()/配置['主解析度'][1]))
+    def resizeEvent(self, ev):
+        self.網頁.resize(self.width(), self.height())
+        self.網頁.頁面.setZoomFactor(min(self.width() / 配置['主解析度'][0], self.height() / 配置['主解析度'][1]))
 
 
-    
 主窗口 = 統合窗口()
