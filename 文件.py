3102e24e915f20177835895579d347323d8f1ff7
Rimo <RimoChan@stu.xmu.edu.cn>
2019-02-25 17:15:47 +0800

    刪除一個臨時輸出

--- 文件.py
+++ 文件.py
@@ -9,7 +9,6 @@ def 補充擴展名(文件名, 擴展名表, 路徑='./'):
         return 文件名
     for 擴展名 in 擴展名表:
         if os.path.isfile(f'{路徑}/{文件名}.{擴展名}'):
-            print(f'{文件名}.{擴展名}')
             return f'{文件名}.{擴展名}'
     logging.warning(f'「{文件名}」不存在。')
     return 文件名
