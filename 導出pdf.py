4a143b97bbd45f66e5d5d1e933411c2d952cf044
Rimo <RimoChan@stu.xmu.edu.cn>
2019-03-09 11:07:34 +0800

    升级。 顺便用weasyprint替换pdfkit。

--- 導出pdf.py
+++ 導出pdf.py
@@ -2,7 +2,8 @@ import os
 import re
 import argparse
 
-import pdfkit
+from weasyprint import HTML, CSS
+
 import opencc
 
 import 編譯
@@ -10,7 +11,7 @@ import 編譯
 此處 = os.path.dirname(os.path.abspath(__file__))
 
 cc = opencc.OpenCC('t2s')
-簡化字 = True
+簡化字 = False
 
 參數 = argparse.ArgumentParser(description='劇本文件生成pdf')
 參數.add_argument('--css', type=str, default='./導出組件/樣式.html')
@@ -38,26 +39,30 @@ def 導出(f):
     html = 頭
     a = 編譯.編譯(f)
     for s in a:
-        if '插入圖' in s:
-            html += f'<hr/>{s["插入圖"]}<hr/>'
-        if '旁白' in s:
+        類型 = s['類型']
+        if 類型 == '插入圖':
+            html += f'<div class="插入">{s["插入圖"]}</div>'
+        if 類型 == '旁白':
             旁白 = 打標點(化(s["旁白"]))
-            html += f'<span class="旁白">　　{旁白}</span><br/>\n'
-        if '名' in s:
+            html += f'<p class="旁白">　　{旁白}</p>\n'
+        if 類型=='人物對話':
             代 = 化(s['代'] or s["名"])
             顏 = 化(s['顏'] or '')
             語 = 化(s.get('語', ''))
-            html += f'<span class="代 {s["名"]}">　　{代}</span>'
+            
+            html += f'<p>　　<span class="代 {s["名"]}">{代}</span>'
             if 顏:
                 html += f'<span class="顏">({顏})</span>'
             if 語:
-                html += f'「<span class="語">{語}</span>」'
-            html += f'<br/>'
-
-        if '註釋' in s:
+                html += f'<span class="語">{語}</span>'
+            html += f'</p>'
+        if 類型 == '註釋':
             html += f'<span class="註釋">{s["註釋"]}</span><br/>\n'
-        if '空行' in s:
-            html += '<br/>\n'
+        if 類型 == '函數調用':
+            html += f'<span class="函數調用">{s["原文"]}</span><br/>\n'
+        if '之後的空白' in s:
+            html += '<br/>\n'*s['之後的空白']
+        
 
     return html
 
@@ -79,12 +84,13 @@ if __name__ == '__main__':
                 with open(參數.out, 'w', encoding='utf8') as f2:
                     f2.write(導出(f))
             if pdf:
-                pdfkit.from_string(t, 參數.out, options={
-                    'margin-top': '0in',
-                    'margin-right': '0in',
-                    'margin-bottom': '0in',
-                    'margin-left': '0in',
-                    'encoding': 'UTF-8',
-                })
+                HTML(string=t).write_pdf(參數.out)
+                # pdfkit.from_string(t, 參數.out, options={
+                #     'margin-top': '0in',
+                #     'margin-right': '0in',
+                #     'margin-bottom': '0in',
+                #     'margin-left': '0in',
+                #     'encoding': 'UTF-8',
+                # })
             else:
                 raise Exception('操你媽')
