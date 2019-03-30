0c043553109e87791af8be92fcbf3890cd301015
Rimo <RimoChan@stu.xmu.edu.cn>
2019-03-30 20:21:13 +0800

    導出pdf功能加強

--- 導出pdf.py
+++ 導出pdf.py
@@ -7,8 +7,8 @@ from weasyprint import HTML, CSS
 
 import opencc
 
-import 編譯
-
+import 劇本
+import 角色
 此處 = os.path.dirname(os.path.abspath(__file__))
 
 cc = opencc.OpenCC('t2s')
@@ -22,57 +22,72 @@ cc = opencc.OpenCC('t2s')
 
 if not 參數.css:
     參數.css = [os.path.join(此處, './資源/導出pdf用/紙樣式.css')]
+    
+    
+class 虛狀態:
+    def __init__(self):
+        self.額外信息 = ()
+        self.內容 = {}
+    def 導出(self): 
+        return {'內容':self.內容, '額外信息':self.額外信息}
+    
+class 速讀者(劇本.讀者):
+    def 步進(self):
+        self.狀態=虛狀態()
+        s = self.下一句()
+        類型 = s['類型']
+        if 類型 == '終焉':
+            self.狀態.額外信息 = ('終焉',)
+        if 類型 in ('註釋', '躍點'):
+            self.狀態.內容 = f'<span class="{類型}">{s[類型]}</span><br/>\n'
+        if 類型 == '函數調用':
+            self.狀態.內容 = f'<span class="函數調用">{s["原文"]}</span><br/>\n'
+        if 類型 == '插入圖':
+            self.狀態.內容 = f'<div class="插入">{s["插入圖"]}</div>'
+        if 類型 == '旁白':
+            旁白=self.打標點(self.化(s["旁白"]))
+            self.狀態.內容 = f'<p class="旁白">{旁白}</p>\n'
+        if 類型 == '人物操作':
+            人物名 = s['人物名']
+            目標 = s['目標']
+            if s['操作符'] == '+':
+                self.狀態.內容 = f'<p class="人物操作">{人物名} 穿着 {目標}</p>\n'
+            if s['操作符'] == '|':
+                角色.取角色(人物名).顯示名字 = s['目標']
+                self.狀態.內容 = ''
+        if 類型 in ('人物對話', '人物表情'):
+            替代顯示名字 = 角色.取角色(s['名']).顯示名字
+            名字 = s['代'] or 替代顯示名字 or s['名']
+            self.狀態.內容 = f'<p><span class="代 {s["名"]}">{名字}</span>'
+            if s['顏']:
+                self.狀態.內容 += f'<span class="顏">{s["顏"]}</span>'
+            if 類型=='人物對話':
+                self.狀態.內容 += f'<span class="語">{s["語"]}</span>'
+            self.狀態.內容 += f'</p>'
+            
+        if '之後的空白' in s:
+            self.狀態.內容 += '<br/>\n' * s['之後的空白']
+            
+    def 化(self,s):
+        if 簡化字:
+            return cc.convert(s)
+        return s
+        
+    def 打標點(self,句):
+        句=re.sub('([，。？！…——])', '<span class="標點">\\1</span>', 句)
+        句=re.sub('(「.*?」)', '<span class="引用">\\1</span>', 句)
+        return 句
 
-def 化(s):
-    if 簡化字:
-        return cc.convert(s)
-    return s
-
-
-def 打標點(句):
-    句=re.sub('([，。？！…——])', '<span class="標點">\\1</span>', 句)
-    句=re.sub('(「.*?」)', '<span class="引用">\\1</span>', 句)
-    return 句
-
-
-def 導出(f):
-    head=f'''
+def 導出(文件名):
+    head = f'''
     <head>
         <meta charset="utf8"/>
         {" ".join([f"<link rel='stylesheet' href='file:///{css}'/>" for css in 參數.css])}
     </head>
     '''
-    logging.debug(head)
-    body=''
-    a=編譯.編譯(f)
-    for s in a:
-        類型=s['類型']
-        if 類型 == '插入圖':
-            body += f'<div class="插入">{s["插入圖"]}</div>'
-        if 類型 == '旁白':
-            旁白=打標點(化(s["旁白"]))
-            body += f'<p class="旁白">{旁白}</p>\n'
-        if 類型 == '人物對話':
-            代=化(s['代'] or s["名"])
-            顏=化(s['顏'] or '')
-            語=化(s.get('語', ''))
-
-            body += f'<p><span class="代 {s["名"]}">{代}</span>'
-            if 顏:
-                body += f'<span class="顏">{顏}</span>'
-            if 語:
-                body += f'<span class="語">{語}</span>'
-            body += f'</p>'
-        if 類型 == '註釋':
-            body += f'<span class="註釋">{s["註釋"]}</span><br/>\n'
-        if 類型 == '函數調用':
-            body += f'<span class="函數調用">{s["原文"]}</span><br/>\n'
-        if '之後的空白' in s:
-            body += '<br/>\n' * s['之後的空白']
-
-    return head + f'<body>{body}</body>'
-
-
+    t=list(速讀者(文件名).迭代器())
+    return head + '\n'.join([i['內容'] for i in t])
+    
 if __name__ == '__main__':
     if 參數.play:
         pdf=html=False
@@ -83,13 +98,13 @@ if __name__ == '__main__':
             pdf=True
         elif 參數.out.endswith('pdf'):
             html=True
-
-        with open(參數.play, encoding='utf8') as f:
-            t=導出(f)
-            if html:
-                with open(參數.out, 'w', encoding='utf8') as f2:
-                    f2.write(導出(f))
-            if pdf:
-                HTML(string=t).write_pdf(參數.out)
-            else:
-                raise Exception('操你媽')
+        
+        t = 導出(參數.play)
+        if html:
+            with open(參數.out, 'w', encoding='utf8') as f2:
+                f2.write(t)
+        if pdf:
+            HTML(string=t).write_pdf(參數.out)
+        else:
+            raise Exception('操你媽')
+            
