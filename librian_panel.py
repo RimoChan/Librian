0297205c9d07d7ca9f4ff25cc1af096bbdb29075
Rimo <RimoChan@stu.xmu.edu.cn>
2019-07-10 11:12:42 +0800

    路徑變更

--- librian_panel.py
+++ librian_panel.py
@@ -4,9 +4,8 @@ import subprocess
 import shutil
 import wx
 
-import wxcef
-
-from Librian虛擬機 import 虛擬機環境
+from Librian本體 import wxcef
+from Librian本體.Librian虛擬機 import 虛擬機環境
 
 
 def js(x):
@@ -28,7 +27,7 @@ class 山彥:
             if 虛擬機環境.圖標:
                 圖標路徑 = os.path.relpath(f'{工程路徑}/{虛擬機環境.圖標}', './html面板')
             else:
-                圖標路徑 = '../資源/librian.ico'
+                圖標路徑 = '../Librian本體/資源/librian.ico'
             js(f'v.工程路徑={工程路徑.__repr__()}')
             js(f'v.圖標路徑={圖標路徑.__repr__()}')
             js(f'v.標題={虛擬機環境.標題.__repr__()}')
@@ -40,7 +39,7 @@ class 山彥:
 
     def 開啓工程(self):
         with wx.DirDialog(self.窗口, "选择文件夹") as dlg:
-            dlg.SetPath(os.path.abspath('./project'))
+            dlg.SetPath(os.path.abspath('./Librian本體/project'))
             if dlg.ShowModal() == wx.ID_OK:
                 self.同調(dlg.GetPath())
 
@@ -51,15 +50,16 @@ class 山彥:
                 if os.path.isdir(新工程路徑):
                     alert('已經有這個工程了。')
                     return
-                shutil.copytree('./project/_默認工程', 新工程路徑)
+                shutil.copytree('./Librian本體/project/_默認工程', 新工程路徑)
                 self.同調(新工程路徑)
 
     def 運行(self):
-        os.system(f'""./python36/python"" ./librian.py --project {self.工程路徑}')
-
+        subprocess.Popen(f'cmd /c cd Librian本體 & "../python36/python" librian.py --project {self.工程路徑}')
+        
     def 運行同時編寫(self):
         subprocess.Popen(
-            f'"./python36/python" ./librian.py --project {self.工程路徑} '
+            'cmd /c cd Librian本體 & ' +
+            f'"../python36/python" librian.py --project {self.工程路徑} '
             + '--config "{編寫模式: True}"'
         )
         os.system(f'"{self.工程路徑}/{虛擬機環境.劇本入口}"')
@@ -68,7 +68,7 @@ class 山彥:
         os.system(f'start {self.工程路徑}')
 
     def 生成exe(self):
-        import 構建
+        from Librian本體 import 構建
         if 虛擬機環境.圖標:
             構建.構建工程(self.工程路徑, 虛擬機環境.標題, f'{self.工程路徑}/{虛擬機環境.圖標}')
         else:
@@ -76,12 +76,12 @@ class 山彥:
         alert('好了。')
 
     def 生成html(self):
-        import 虛擬演繹
+        from Librian本體 import 虛擬演繹
         虛擬演繹.生成虛擬核心()
         alert('好了。')
 
 
-app, 瀏覽器 = wxcef.group(title='librian面板', url='file:///html面板/面板.html', icon='./資源/librian.ico', size=(800, 450))
-真山彥 = 山彥(app.frame)
+app, 瀏覽器=wxcef.group(title = 'librian面板', url = 'file:///html面板/面板.html', icon = './Librian本體/資源/librian.ico', size = (800, 450))
+真山彥=山彥(app.frame)
 app.frame.set_browser_object("山彥", 真山彥)
 app.MainLoop()
