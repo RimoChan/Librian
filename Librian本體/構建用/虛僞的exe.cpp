7ced7ec4f21fbfe546a79bb147ae65a2b3eae112
Rimo <RimoChan@stu.xmu.edu.cn>
2019-10-22 15:15:01 +0800

    重新用C寫了一遍，爲了解決依賴dll之類的。

--- Librian本體/構建用/虛僞的exe.cpp
+++ Librian本體/構建用/虛僞的exe.cpp
@@ -1,17 +1,39 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cassert>
+
+char *join(const char *s1, const char *s2) {
+    char *result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
 int main(int argc, char *argv[]) {
-    string s = string(argv[0]);
-    string ss = s.substr(s.rfind('\\') + 1, s.rfind('.') - s.rfind('\\') - 1);
+    char *s = argv[0];
+    printf("%s\n", s);
+    char *a = strrchr(s, '\\') + 1, *b = strrchr(s, '.');
+    b[0] = 0;
+    printf("<%s>\n", a);
+    char *name = join(join("_", a), ".kuzu");
+    printf("[%s]\n", name);
 
-    cout << s << endl;
-    cout << "<" << ss << ">" << endl;
+    FILE *f = fopen(name, "r");
+    assert(f != 0);
 
-    ifstream t("_" + ss + ".kuzu");
-    std::string q((std::istreambuf_iterator<char>(t)),
-        std::istreambuf_iterator<char>());
+    int length;
+    char *data;
+    fseek(f, 0, SEEK_END);
+    length = ftell(f);
+    data = (char *)malloc((length + 1) * sizeof (char));
+    rewind(f);
+    length = fread(data, 1, length, f);
+    data[length] = 0;
+    fclose(f);
 
-    system(q.c_str());
+    printf("%s\n", data);
+
+    system(data);
 }
