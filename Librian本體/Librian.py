8f728cadf91fd593b9ced6b5319291c59d756038
Rimo <RimoChan@stu.xmu.edu.cn>
2019-10-24 18:38:01 +0800

    使用fire簡化入口。

--- Librian本體/Librian.py
+++ Librian本體/Librian.py
@@ -1,30 +1,29 @@
 import os
 import logging
-import argparse
+
 import yaml
+import fire
 
 import 環境
 
-參數 = argparse.ArgumentParser(description='啓動Librian。')
-參數.add_argument('--project', type=str, required=True)
-參數.add_argument('--config', type=str)
-參數 = 參數.parse_args()
-
 import Librian虛擬機
 import Librian虛擬機.虛擬機環境
-Librian虛擬機.虛擬機環境.加載配置(參數.project)
 
-if 參數.config:
-    環境.導入全局配置(yaml.load(參數.config))
-    
 
-try:
-    os.mkdir(f'{環境.工程路徑}/存檔資料')
-except Exception as e:
-    logging.debug('已有存檔。')
-    
-import 窗口
-    
-app = 窗口.app()
+def librian_main(project, config=None):
+    Librian虛擬機.虛擬機環境.加載配置(project)
+    if config:
+        環境.導入全局配置(config)
 
-app.MainLoop()
+    try:
+        os.mkdir(f'{環境.工程路徑}/存檔資料')
+    except Exception as e:
+        logging.debug('已有存檔。')
+
+    import 窗口
+
+    app = 窗口.app()
+    app.MainLoop()
+
+
+fire.Fire(librian_main)
