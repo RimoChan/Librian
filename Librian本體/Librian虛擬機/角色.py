15d1fdc6599da23ea04b896127e04740def5cd6b
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-05-25 15:35:12 +0800

    更換簡單loader防止圖層名變成數字，以及略作修改。

--- Librian本體/Librian虛擬機/角色.py
+++ Librian本體/Librian虛擬機/角色.py
@@ -20,15 +20,16 @@ def 取角色(名字):
 
 
 def 導入有立繪的角色():
+    工程路徑, 臨時立繪文件夾, psd立繪路徑 = 虛擬機環境.工程路徑, 虛擬機環境.臨時立繪文件夾, 虛擬機環境.psd立繪路徑
     try:
-        工程路徑, 臨時立繪文件夾, psd立繪路徑 = 虛擬機環境.工程路徑, 虛擬機環境.臨時立繪文件夾, 虛擬機環境.psd立繪路徑
         with 讀txt.讀(f'{虛擬機環境.psd立繪路徑}/映射.yaml') as f:
-            映射 = yaml.load(f)
-            if 映射:
-                for 角色名 in 映射:
+            映射表 = yaml.load(f, Loader=yaml.BaseLoader)
+            if 映射表:
+                for 角色名 in 映射表:
                     if not os.path.isdir(f'{工程路徑}/{臨時立繪文件夾}/{角色名}'):
                         psd拆包.拆包(f'{psd立繪路徑}/{角色名}.psd', f'{工程路徑}/{臨時立繪文件夾}')
-                    角色(角色名, 映射[角色名])
+                    logging.info(f'角色「{角色名}」的立繪: \n{映射表[角色名]}')
+                    角色(角色名, 映射表[角色名])
             for i in os.listdir(psd立繪路徑):
                 if i.endswith('.png'):
                     前名 = os.path.basename(os.path.splitext(i)[0])
@@ -57,8 +58,8 @@ class 角色:
             self.衣圖層 = 立繪表['衣']
             self.顏圖層 = 立繪表['顏']
             self.固有縮放 = 立繪表.get('縮放', 1)
-            self.現顏 = '_默認'
-            self.現衣 = '_默認'
+            self.現顏 = None
+            self.現衣 = None
             self.現特效 = None
         else:
             logging.warning(f'新建了沒有立繪的角色「{名字}」')
