b9b1ee9859a6cb3adbb59f7aed9c0e872fd7cc0a
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-10-18 13:37:37 +0800

    更新版本號，刪除dist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='librian',
-    version='0.0.1',
+    version='0.0.2',
     author='RimoChan',
     author_email='the@librian.net',
     description='librian',
