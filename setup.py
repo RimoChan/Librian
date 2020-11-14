e27e8441ea501c9a2e7f9ac5a3e55b099bb40a2e
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-11-14 16:24:13 +0800

    更新synthetic的css，把背景換成webp

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='librian',
-    version='0.0.2',
+    version='0.0.3',
     author='RimoChan',
     author_email='the@librian.net',
     description='librian',
